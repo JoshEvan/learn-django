46129eb6b3f33a1441c91db423e20b80b51996ae
Joshua Evan Arijanto <joshua.arijanto@gmail.com>
2020-05-26 00:53:47 +0700

    add add items

--- src/items/urls.py
+++ src/items/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from .views import(
     IndexView,
-    DetailView
+    DetailView,
+    CreateView
 )
 
 app_name = "items"
 urlpatterns=[
     path('',IndexView.as_view(),name="index"),
-    path('detail/<int:idItem>', DetailView.as_view(), name="detail")
+    path('detail/<int:idItem>', DetailView.as_view(), name="detail"),
+    path('create/',CreateView.as_view(),name="create")
 ]
--- src/items/views.py
+++ src/items/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+from django.urls import reverse_lazy
 from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
 class ItemView():
@@ -14,12 +16,12 @@ class ItemView():
         return obj
         
 
-
 class IndexView(View):
     template_name = "items/index.html"
     queryset = Item.objects.all()
     context = {
-        "object_list":queryset
+        "object_list":queryset,
+        "create_url": reverse_lazy('items:create')
     }
 
     def get(self,request,*args,**kwargs):
@@ -35,4 +37,27 @@ class DetailView(ItemView,View):
             context["obj"] = obj
 
         return render(request, self.template_name,context)
+
+class CreateView(View):
+    template_name = "items/create.html"
+    
+    def get(self, request, *args, **kwargs):
+        form = ItemForm()
+        context = {
+            'form': form
+        }
+        return render(request,self.template_name, context)
+    
+    def post(self,request, *args, **kwargs):
+        form = ItemForm(request.POST)
+        context = {}
+
+        if form.is_valid():
+            form.save()
+            form = ItemForm()
+            return redirect(reverse_lazy('items:index'))
+        
+        context['form'] = form
+
+        return render(request, self.template_name, context)
 
